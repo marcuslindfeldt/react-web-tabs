6b6bda7c34704b10a771d692a56be6efee401ec9
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2017-04-01 23:03:57 +0200

    Basic example styles

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,37 +1,51 @@
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-export default () => (
-  {
-    entry: './src/index.js',
+const minified = process.env.NODE_ENV === 'production' ? '.min' : '';
 
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'react-web-tabs.js',
-      libraryTarget: 'umd',
-      library: 'react-web-tabs',
+export default () => ({
+  entry: [
+    './src/index.js',
+    './styles/style.css',
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `react-web-tabs${minified}.js`,
+    libraryTarget: 'umd',
+    library: 'react-web-tabs',
+  },
+
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
     },
+  },
 
-    externals: {
-      react: {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
-    },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
+    ],
+  },
 
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
-        },
-      ],
-    },
-  }
-);
+  plugins: [
+    new ExtractTextPlugin(`react-web-tabs${minified}.css`),
+  ],
+});
